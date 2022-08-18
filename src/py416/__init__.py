5df1301adf7a947a1dfdd0164f2d16f77a036b31
Lance <foresth@bditest.com>
2022-08-18 15:59:02 -0600

    0.4

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,8 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-08-18
-Version: 0.3
+Version: 0.4
 
 A collection of my custom-built functions
 '''
-from general import *
-
+from .general import *
