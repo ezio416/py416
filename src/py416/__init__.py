088d0c23613e36113cc1d927f96e0a49b72c316e
Lance <foresth@bditest.com>
2022-09-01 12:55:56 -0600

    0.6: fix listdir all(), init File class

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,10 +2,10 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-08-19
-Version: 0.5
+Updated: 2022-09-01
+Version: 0.6
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = (0, 5)
+__version__ = 0, 6
