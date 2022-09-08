129ba75212ba33d367e8228aec44497fa33f98fe
Lance <foresth@bditest.com>
2022-09-08 16:04:15 -0600

    0.10: parent changes

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-08
-Version: 0.9
+Version: 0.10
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 9
+__version__ = 0, 10
