8203b4ecb15ac6aebc31ec90076d1d037cdb800d
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-08 20:56:50 -0600

    0.11

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-08
-Version: 0.10
+Version: 0.11
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 10
+__version__ = 0, 11
