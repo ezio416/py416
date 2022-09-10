883c2045767e1841b8c8b8cf45e407f88d86347c
Ezio <ezezio416@gmail.com>
2022-09-10 15:45:06 -0600

    0.16: isroot/root, stem(dir), +pathjoin, splitpath

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,10 +2,10 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-09
-Version: 0.15
+Updated: 2022-09-10
+Version: 0.16
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 15
+__version__ = 0, 16
