a53478de5801039398d9b68b631d98e55e49177b
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-09 22:50:14 -0600

    0.14: splitpath, var names, small other things

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-09
-Version: 0.13
+Version: 0.14
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 13
+__version__ = 0, 14
