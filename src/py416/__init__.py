5b42102f9e2ffb3bef24b8848bac3a651a992809
Ezio <ezezio416@gmail.com>
2022-09-13 21:31:53 -0600

    0.24: +getpath, -realpath, fix path things, ctime

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,10 +2,10 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-12
-Version: 0.23
+Updated: 2022-09-13
+Version: 0.24
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 23
+__version__ = 0, 24
