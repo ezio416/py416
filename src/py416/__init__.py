01a0c070468d76496a4275bf8a6094cb5f47a498
Lance <foresth@bditest.com>
2022-09-16 08:26:47 -0600

    0.28: renamed paths to files

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,10 +2,10 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-15
-Version: 0.27
+Updated: 2022-09-16
+Version: 0.28
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 27
+__version__ = 0, 28
