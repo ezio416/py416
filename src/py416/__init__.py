bd6da8770194a38de11f345187ce5e907531c270
Ezio <ezezio416@gmail.com>
2022-09-16 00:48:32 -0600

    0.27: copy+move overhaul

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-15
-Version: 0.26.1
+Version: 0.27
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 26, 1
+__version__ = 0, 27
