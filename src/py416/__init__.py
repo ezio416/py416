89d8e657e94001958156dd65712e6cfa1a45bd1b
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-16 21:01:17 -0600

    0.31: removed another path exist check

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-16
-Version: 0.30
+Version: 0.31
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 30
+__version__ = 0, 31
