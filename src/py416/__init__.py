f3fd6e794c19c10dee13be287aadd0f8a41cb4c5
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-17 11:52:01 -0600

    0.32: fix listdir default

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,10 +2,10 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-16
-Version: 0.31
+Updated: 2022-09-17
+Version: 0.32
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 31
+__version__ = 0, 32
