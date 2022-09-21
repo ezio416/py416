76f540744b879166a6d5e6b6b3a226fa9056699a
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-21 16:32:07 -0600

    0.36: simplified copy/move logic and input checks

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-21
-Version: 0.35
+Version: 0.36
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 35
+__version__ = 0, 36
