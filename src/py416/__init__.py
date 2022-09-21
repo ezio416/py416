f87c9a72a9dede8d7ed22b99bb469a456740e2a9
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-21 16:43:41 -0600

    0.37: more input checks, some docs

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,9 +3,9 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-21
-Version: 0.36
+Version: 0.37
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 36
+__version__ = 0, 37
