b4f1839d1333329bd32f96d3703d2934cd8098d8
Ezio <fohoover@gmail.com>
2022-09-21 12:00:55 -0600

    0.35: path fixes, special cases, not using abspath

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,10 +2,10 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-20
-Version: 0.34
+Updated: 2022-09-21
+Version: 0.35
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 34
+__version__ = 0, 35
