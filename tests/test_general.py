2be63f60b70d6636cc250bb36492f1694ab4696f
Ezio <fohoover@gmail.com>
2022-09-22 10:59:29 -0600

    param tests, added some

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,98 +1,130 @@
 import os
 import sys
 
+import pytest
+
 sys.path.append(f'{os.path.dirname(os.path.realpath(__file__))}/..')
 
 import src.py416.general as g
 from src.py416.files import File
 
-def test_gettype():
-    assert g.gettype(True)  == 'bool'
-    assert g.gettype(False) == 'bool'
-    
-    assert g.gettype(bytes())         == 'bytes'
-    assert g.gettype(b'')             == 'bytes'
-    assert g.gettype(b'hello')        == 'bytes'
-    assert g.gettype(b'\xE2\x82\xAC') == 'bytes'
-    
-    assert g.gettype(bytearray()) == 'bytearray'
-    
-    assert g.gettype(complex()) == 'complex'
-    
-    assert g.gettype(dict())                   == 'dict'
-    assert g.gettype({})                       == 'dict'
-    assert g.gettype({1: 2})                   == 'dict'
-    assert g.gettype({'a': True, True: False}) == 'dict'
-    
-    assert g.gettype(float())    == 'float'
-    assert g.gettype(0.0)        == 'float'
-    assert g.gettype(-0.0)       == 'float'
-    assert g.gettype(12.3456789) == 'float'
-    assert g.gettype(-9001.001)  == 'float'
-    assert g.gettype(12.)        == 'float'
-    assert g.gettype(-9001.)     == 'float'
-    assert g.gettype(1000 / 11)  == 'float'
-    
-    assert g.gettype(frozenset()) == 'frozenset'
-    
-    assert g.gettype(int())     == 'int'
-    assert g.gettype(0)         == 'int'
-    assert g.gettype(-0)        == 'int'
-    assert g.gettype(123)       == 'int'
-    assert g.gettype(-123456)   == 'int'
-    
-    assert g.gettype(list())        == 'list'
-    assert g.gettype([])            == 'list'
-    assert g.gettype(['j', 26, {}]) == 'list'
-    
-    assert g.gettype(memoryview(b'')) == 'memoryview'
-    
-    assert g.gettype(None) == 'NoneType'
-    
-    assert g.gettype(range(1)) == 'range'
-    
-    assert g.gettype(set())           == 'set'
-    assert g.gettype({1, 2, 'horse'}) == 'set'
-    
-    assert g.gettype(str())           == 'str'
-    assert g.gettype('')              == 'str'
-    assert g.gettype('mystring')      == 'str'
-    assert g.gettype(f'{69420}blaze') == 'str'
-    assert g.gettype(r'\example\n')   == 'str'
-    
-    assert g.gettype(tuple())         == 'tuple'
-    assert g.gettype(())              == 'tuple'
-    assert g.gettype((1, 2, 'yeah'))    == 'tuple'
-    assert g.gettype((3, f'g', {6, 7})) == 'tuple'
-    assert g.gettype((True, 98))      == 'tuple'
-    
-    print(g.gettype(File))
+@pytest.mark.parametrize('i,o', [
+    (bool(), 'bool'),
+    (True, 'bool'),
+    (False, 'bool'),
 
-def test_month2num():
-    # print([g.month2num('aug')])
-    pass
+    (bytes(), 'bytes'),
+    (b'', 'bytes'),
+    (b'hello', 'bytes'),
+    (b'\xE2\x82\xAC', 'bytes'),
 
-def test_secmod():
-    pass
+    (bytearray(), 'bytearray'),
+
+    (complex(), 'complex'),
+
+    (dict(), 'dict'),
+    ({}, 'dict'),
+    ({1: 2}, 'dict'),
+    ({'a': 416, True: False}, 'dict'),
+
+    (float(), 'float'),
+    (0.0, 'float'),
+    (-0.0, 'float'),
+    (12.3456789, 'float'),
+    (-9001.001, 'float'),
+    (12., 'float'),
+    (-9001., 'float'),
+    (1000 / 11, 'float'),
+    (1000 / 10, 'float'),
+
+    (frozenset(), 'frozenset'),
+
+    (int(), 'int'),
+    (0, 'int'),
+    (-0, 'int'),
+    (123, 'int'),
+    (-123456, 'int'),
+
+    (list(), 'list'),
+    ([], 'list'),
+    (['j', 26, {}], 'list'),
+
+    (memoryview(b''), 'memoryview'),
+
+    (None, 'NoneType'),
+
+    (range(1), 'range'),
+
+    (set(), 'set'),
+    ({1, 2, 'horse'}, 'set'),
+
+    (str(), 'str'),
+    ('', 'str'),
+    ('mystring', 'str'),
+    (f'{69420}blaze', 'str'),
+    (r'\example\n', 'str'),
+
+    (tuple(), 'tuple'),
+    ((), 'tuple'),
+    ((45,), 'tuple'),
+    ((1, 2, 'yeah'), 'tuple'),
+    ((3, f'g', {6, 7}), 'tuple'),
+    ((True, 98), 'tuple'),
+
+    (File, 'type'),
+])
+def test_gettype(i, o):
+    assert g.gettype(i) == o
+
+@pytest.mark.parametrize('i,o', [
+    ('january', '01'),
+    ('February', '02'),
+    ('mArCh', '03'),
+    ('APRIL', '04'),
+    ('may', '05'),
+    ('junE', '06'),
+    ('july', '07'),
+    ('august', '08'),
+    ('SEPtember', '09'),
+    ('ocTOber', '10'),
+    ('november', '11'),
+    ('December', '12'),
+    ('', ''),
+    ('jan', ''),
+    ('greq', ''),
+    ('March_2022', ''),
+    ('AprilApril', ''),
+    ('maytheforce', ''),
+    ('timeInJune', ''),
+    ('where july', ''),
+])
+def test_month2num(i, o):
+    assert g.month2num(i) == o
+
+@pytest.mark.parametrize('i,a,o', [
+    (3, '', ('03s', 3, 0, 0, 0)),
+    (505, ' ', ('08m 25s', 25, 8, 0, 0)),
+    (12345, '', ('03h25m45s', 45, 25, 3, 0)),
+    (12345678, '--', ('142d--21h--21m--18s', 18, 21, 21, 142)),
+    (-46834, ',', ('13h,34s', 34, 0, 13, 0)),
+])
+def test_secmod(i, a, o):
+    assert g.secmod(i, sep=a) == o
 
 def test_timestamp():
-    # i = 1
-    # for a in 0, 1:
-    #     for b in 0, 1:
-    #         for c in 0, 1:
-    #             for d in 0, 1:
-    #                 for e in 0, 1:
-    #                     for f in 0, 1:
-    #                         print(str(i).zfill(2), g.timestamp(a, b, c, d, e, f))
-    #                         i += 1
-    #         print('\n')
     pass
 
-def test_unpack():
-    pass
-
-test_gettype()
-test_month2num()
-test_secmod()
-test_timestamp()
-test_unpack()
+@pytest.mark.parametrize('i,o', [
+    ('', ('')),
+    ([], ()),
+    ([1], (1,)),
+    ([1, [2]], (1, 2)),
+    ([1, (2, [3])], (1, 2, 3)),
+    ((), ()),
+    ((1,), (1,)),
+    ((1, (2)), (1, 2)),
+    ((1, [2, (3)]), (1, 2, 3)),
+    (((((([[[['arf', (65432, 'woof')]], 65]])), 6))), ('arf', 65432, 'woof', 65, 6)),
+])
+def test_unpack(i, o):
+    assert g.unpack(i) == o
