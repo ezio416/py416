0141015e000e496991f17808c08375a2cafaa9f7
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-22 19:48:24 -0600

    0.39: init makefile, docs

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,10 +3,10 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-22
-Version: 0.38
+Version: 0.39
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 38
+__version__ = 0, 39
 v = __version__
