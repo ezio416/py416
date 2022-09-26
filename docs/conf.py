86c7fb271b9355ee7535dc29521240051acae652
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-26 12:54:02 -0600

    piccolo theme, moved things, del comments

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-# import src.py416
-# import src.py416.files
-# import src.py416.general
-# import src.py416.variables
+sys.path.append(os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -21,19 +17,18 @@ author = 'Ezio416'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+add_module_names = False
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
-
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']
