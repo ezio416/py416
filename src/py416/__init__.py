5fc9acf109be8bbe476ff80698dc3978b24f08d4
Ezio <37951898+ezio416@users.noreply.github.com>
2022-09-26 16:45:10 -0600

    0.41: fix move file overwrite, some docs

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,10 +3,10 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-26
-Version: 0.40
+Version: 0.41
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 40
+__version__ = 0, 41
 v = __version__
