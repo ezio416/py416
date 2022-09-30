fa289e89e92ed801f9df6631c80742a1a440c1b3
Ezio <fohoover@gmail.com>
2022-09-30 14:12:30 -0600

    0.45: docs, listdir recency takes str or num

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -3,10 +3,10 @@ Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
 Updated: 2022-09-30
-Version: 0.44
+Version: 0.45
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 44
+__version__ = 0, 45
 v = __version__
