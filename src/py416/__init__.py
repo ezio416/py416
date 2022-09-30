b2c94fe40484dced3e3b34c58e426bdfdbcc6ac1
Ezio <fohoover@gmail.com>
2022-09-30 12:37:20 -0600

    0.44: added options to listdir, not auto tested

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,11 +2,11 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-27
-Version: 0.43
+Updated: 2022-09-30
+Version: 0.44
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 43
+__version__ = 0, 44
 v = __version__
