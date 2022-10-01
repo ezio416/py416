6eafe6f5346004196c192f64f22b18c134db09f4
Ezio <37951898+ezio416@users.noreply.github.com>
2022-10-01 01:35:29 -0600

    0.46: listdir looking good so far

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,11 +2,11 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-09-30
-Version: 0.45
+Updated: 2022-10-01
+Version: 0.46
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 45
+__version__ = 0, 46
 v = __version__
