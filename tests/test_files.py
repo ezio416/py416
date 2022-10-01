f7ac21b63f3f656effb504560ca955dfb3f4295b
Ezio <37951898+ezio416@users.noreply.github.com>
2022-10-01 01:20:28 -0600

    recency

--- tests/test_files.py
+++ tests/test_files.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from pprint import pprint
 import sys
+import time
 
 import pytest
 import pytest_check as check
@@ -10,6 +11,7 @@ install()
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import src.py416.files as p4f
+import src.py416.variables as p4v
 
 # def test_File(tmp_path):
 #     str_path = str(tmp_path).replace('\\', '/')
@@ -207,9 +209,57 @@ def test_listdir_search(tmp_path):
     for file in files2:
         check.is_in(file, tree)
 
-# def test_listdir_recency(tmp_path):
-#     str_path = str(tmp_path).replace('\\', '/')
-#     os.chdir(str_path)
+def test_listdir_recency(tmp_path):
+    str_path = str(tmp_path).replace('\\', '/')
+    os.chdir(str_path)
+    sp = str_path
+    files = []
+    nums = (1, 2, 3, 4, 5)
+    dirs = (f'{str_path}/d{num}' for num in nums)
+    now = time.time()
+    for dir in dirs:
+        os.makedirs(dir)
+        files += [f'{dir}/f{num}' for num in nums]
+    for file in files:
+        with open(file, 'a') as f:
+            f.write(f'my name is {file}')
+        if 'd1' in file or 'f1' in file:  # 1970
+            os.utime(file, (1, 1))
+        if 'd2' in file or 'f2' in file:  # 40 years ago
+            os.utime(file, tuple([now - 1_262_304_000] * 2))
+        if 'f3' in file:  # a billion seconds ago
+            os.utime(file, (now - 1e9, now - 1e9))
+        if 'd4' in file:  # an hour ago
+            os.utime(file, (now - 3600, now - 3600))
+
+    tmp = p4f.listdir(dirs=False, recursive=True)  # all files
+    check.equal(len(tmp), 25)
+    (check.is_in(file, tmp) for file in files)
+    
+    tmp = p4f.listdir(dirs=False, recursive=True, recency=1e3)  # files for which we didn't change the modify date
+    check.equal(len(tmp), 4)
+    files_unmodified = [f'{sp}/d{a}/f{b}' for a in (3, 5) for b in (4, 5)]
+    (check.is_in(file, tmp) for file in files_unmodified)
+    
+    tmp = p4f.listdir(dirs=False, recursive=True, recency=4000)  # including an hour
+    check.equal(len(tmp), 9)
+    files_hour = files_unmodified + [f'{sp}/d4/f{num}' for num in nums]
+    (check.is_in(file, tmp) for file in files_hour)
+    
+    tmp = p4f.listdir(dirs=False, recursive=True, recency='11575d')  # including a billion seconds
+    check.equal(len(tmp), 13)
+    files_2001 = files_hour + [f'{sp}/d{num}/f3' for num in (1, 2, 3, 5)]
+    (check.is_in(file, tmp) for file in files_2001)
+    
+    tmp = p4f.listdir(dirs=False, recursive=True, recency='15000d17h3m59s')  # including 40 years
+    check.equal(len(tmp), 20)
+    files_40years = files_2001 + [f'{sp}/d2/f{b}' for b in (1, 2, 4, 5)] + [f'{sp}/d{a}/f2' for a in (1, 3, 5)]
+    (check.is_in(file, tmp) for file in files_40years)
+    
+    tmp = p4f.listdir(dirs=False, recursive=True, recency=2e9)  # including 1970 (should now be all)
+    check.equal(len(tmp), 25)
+    files_1970 = files_40years + [f'{sp}/d1/f{b}' for b in (1, 4, 5)] + [f'{sp}/d{a}/f1' for a in (3, 5)]
+    (check.is_in(file, tmp) for file in files_1970)
 
 # def test_log(tmp_path):
 #     str_path = str(tmp_path).replace('\\', '/')
