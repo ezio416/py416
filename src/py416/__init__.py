9926a366a5f869cb390efbf0e3a9ee4e1d2ee710
Ezio <ezezio416@gmail.com>
2022-10-03 13:57:56 -0600

    0.47: listdir dict

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,11 +2,11 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-10-01
-Version: 0.46
+Updated: 2022-10-03
+Version: 0.47
 
 A collection of my custom-built functions
 '''
 from .general import *
-__version__ = 0, 46
+__version__ = 0, 47
 v = __version__
