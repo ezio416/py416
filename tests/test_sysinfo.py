d5681a4cb48593ede9e0f502fca743ff83339378
Ezio <37951898+ezio416@users.noreply.github.com>
2022-10-12 12:01:08 -0600

    init sysinfo

---
+++ tests/test_sysinfo.py
@@ -0,0 +1,26 @@
+import os
+from pprint import pprint
+import sys
+import time
+
+import pytest
+import pytest_check as check
+from rich.traceback import install
+install()
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import src.py416.sysinfo as p4s
+
+# pprint(p4s.cpu())
+# pprint(p4s.disks(), width=1)
+# pprint(p4s.ram(), width=1)
+# pprint(p4s.system(), width=100)
+# pprint(p4s.users(), width=100)
+pprint(p4s.full(), width=1)
+
+# print(p4s.cpu(json=True))
+# print(p4s.disks(json=True))
+# print(p4s.ram(json=True))
+# print(p4s.system(json=True))
+# print(p4s.users(json=True))
+# print(p4s.full(json=True))
