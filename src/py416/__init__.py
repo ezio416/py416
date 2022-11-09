d15b8753a67ddad81160439b417fed648857fffc
Ezio <fohoover@gmail.com>
2022-11-09 16:13:36 -0700

    0.53 pprint, req py <3.11

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,11 +2,11 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-10-12
-Version: 0.52
+Updated: 2022-11-09
+Version: 0.53
 
 A collection of various functions
 '''
 from .general import *
-__version__ = 0, 52
+__version__ = 0, 53
 v = __version__
