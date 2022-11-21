956302e99c7a194ecd49976dd7528d49b5d2e3b9
Ezio <37951898+ezio416@users.noreply.github.com>
2022-11-21 13:04:11 -0700

    0.56 +lineno

--- src/py416/__init__.py
+++ src/py416/__init__.py
@@ -2,11 +2,11 @@
 Name:    py416
 Author:  Ezio416
 Created: 2022-08-15
-Updated: 2022-11-11
-Version: 0.55
+Updated: 2022-11-21
+Version: 0.56
 
 A collection of various functions
 '''
 from .general import *
-__version__ = 0, 55
+__version__ = 0, 56
 v = __version__
